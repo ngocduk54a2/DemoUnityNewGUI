af122a37504bb44cd8231c1f41c6deca44f9ebf2
framgia <framgia@framgias-MacBook-Pro.local>
2015-03-26 17:31:18 +0700

    init

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void clickPlay() {
+		Application.LoadLevel ("Play");
+	}
+
+	public void clickOption() {
+		Application.LoadLevel ("Option");
+	}
+}	
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+	public GameObject resume;
+	public GameObject back;
+	public bool isPause = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void clickPause() {
+		if (!isPause) {
+			isPause = true;
+			resume.SetActive(true);
+			back.SetActive(true);
+		}
+	}
+
+	public void clickResume() {
+		if (isPause) {
+			isPause = false;
+			resume.SetActive(false);
+			back.SetActive(false);
+		}
+	}
+
+	public void clickBack() {
+		Application.LoadLevel ("Menu");
+	}
+}
